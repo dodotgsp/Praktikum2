c159f5c5325df0872dc19740036336a1b1e32851
dodotgsp <dodotguntur19@gmail.com>
2021-11-05 16:33:37 +0700

    file readme

---
+++ tugas.py
@@ -0,0 +1,7 @@
+print("Menghitung luas lingkaran dan keliling lingkaran")
+r=int(input("masukan jari-jari lingkaran: "))
+phi= 22/7
+L = phi * (r*r)
+K = 2*phi*r
+print ("luas lingkaran dengan jari-jari {} adalah {}".format(r,L))
+print ("keliling lingkaran dengan jari-jari {} adalah {}".format(r,K))
